01e0f424970271ed9a29342b82bbe12cbc253af2
Lucas Cabral <107146157+lucas13cl@users.noreply.github.com>
2023-06-20 16:00:13 -0300

    Add files via upload

---
+++ pasta.js
@@ -0,0 +1,28 @@
+var peso = prompt("Digite seu peso");
+var altura = prompt("Digite sua altura");
+var imc = (peso/(altura*altura));
+
+alert("Seu imc é: " + imc);
+
+if(imc<17){
+    alert("Muito abaixo do peso");
+}else if(imc>17 && imc<=18.49){
+    alert("Abaixo do peso");
+}else if(imc>18,5 && imc<=24.99){
+    alert("Peso normal");
+}else if(imc>25 && imc<=29.99){
+        alert("Acima do peso");
+}else if (imc>30 && imc<=34.99){
+    alert("Obesidade I");
+}else if (imc>35 && imc<=39.99){
+    alert("Obesidade II");
+}else if (imc>40) {
+    alert("Obesidade III");
+}
+
+    
+
+
+
+
+    
